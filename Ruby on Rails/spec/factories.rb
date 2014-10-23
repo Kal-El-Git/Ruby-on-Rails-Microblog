106f4897954ce6cd5e358d2410fba2e262d8b087
Clark <song111@illinois.edu>
2014-10-22 23:10:03 -0500

    testing

--- Ruby on Rails/spec/factories.rb
+++ Ruby on Rails/spec/factories.rb
@@ -5,4 +5,18 @@ FactoryGirl.define do
 		password "ruby"
 		password_confirmation "ruby"
 	end
+    
+    factory :user2 do
+		name "Young2"
+		email "ruby2@rails.com"
+		password "ror"
+		password_confirmation "ror"
+	end
+    
+    factory :user3 do
+		name "Young3"
+		email "ruby3@rails.com"
+		password "test"
+		password_confirmation "test"
+	end
 end
